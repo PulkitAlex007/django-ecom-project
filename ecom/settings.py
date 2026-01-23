21f4285e6323b35ebc866b33dd041d0442913806
Pulkit <shahpulkit@gmail.com>
2026-01-23 21:19:33 +0530

    media image working

--- ecom/settings.py
+++ ecom/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2=xg1tlatdg+^zdd-qbyxxk!m9oj!b+%#$4ajsrmo#py2iwf+v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
